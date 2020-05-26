1b9a894911cf9851166376181f8fc02cbaa06771
Paul-Vincent Roll <paviro@me.com>
2020-05-26 21:57:40 +0200

    Added podlove javascript

--- footer.php
+++ footer.php
@@ -30,5 +30,8 @@
 
     <!-- Custom scripts for this template -->
     <script src="<?= html(path2uri(__DIR__."/js/creative.js")) ?>"></script>
+    
+     <!-- Podlove -->
+    <script src="//podlove.fm4f.org/embed.js"></script>
   </body>
 </html>
